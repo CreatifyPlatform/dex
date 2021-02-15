b66f1c64c7c846f493506a1d7e401f494a5de162
Shachindra <shachindra.spidey@gmail.com>
2021-02-15 22:32:35 +0530

    Updated Address

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -47,7 +47,7 @@ const EXTERNAL_LINKS = {
   '/developer-resources': 'https://serum-academy.com/en/developer-resources/',
   '/explorer': 'https://explorer.solana.com',
   '/srm-faq': 'https://projectserum.com/srm-faq',
-  '/swap': 'https://swap.projectserum.com',
+  '/swap': 'https://creatifyswap.netlify.app',
 };
 
 export default function TopBar() {
--- src/utils/wallet.tsx
+++ src/utils/wallet.tsx
@@ -6,7 +6,7 @@ import { useLocalStorageState } from './utils';
 import { WalletContextValues } from './types';
 
 export const WALLET_PROVIDERS = [
-  { name: 'sollet.io', url: 'https://www.sollet.io' },
+  { name: 'Creatify', url: 'https://creatifywallet.netlify.app' },
 ];
 
 const WalletContext = React.createContext<null | WalletContextValues>(null);
@@ -16,11 +16,11 @@ export function WalletProvider({ children }) {
 
   const [savedProviderUrl, setProviderUrl] = useLocalStorageState(
     'walletProvider',
-    'https://www.sollet.io',
+    'https://creatifywallet.netlify.app',
   );
   let providerUrl;
   if (!savedProviderUrl) {
-    providerUrl = 'https://www.sollet.io';
+    providerUrl = 'https://creatifywallet.netlify.app';
   } else {
     providerUrl = savedProviderUrl;
   }
